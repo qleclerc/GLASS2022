c484d23ae374a413dae2fecafba50752bfa9d23c
Quentin Leclerc <quentin.leclerc@lshtm.ac.uk>
2022-12-27 10:14:16 +0100

    add xlsx

--- compilation_code.R
+++ compilation_code.R
@@ -1,6 +1,7 @@
 
 library(here)
 library(dplyr)
+library(openxlsx)
 
 all_files <- list.files(here::here("raw_data"))
 
@@ -9,13 +10,13 @@ compiled_dataset <- data.frame()
 for(file_i in all_files){
   
   data <- read.csv(here::here("raw_data", file_i), skip = 15)
-  
   compiled_dataset <- rbind(compiled_dataset, data)
   
 }
 
 compiled_dataset <- compiled_dataset %>%
-  mutate_at(vars(Year, TotalSpecimenIsolates, InterpretableAST,
+  dplyr::mutate_at(vars(Year, TotalSpecimenIsolates, InterpretableAST,
                  Resistant, PercentResistant), as.numeric)
 
 write.csv(compiled_dataset, "compiled_WHO_GLASS_2022.csv", row.names = F)
+openxlsx::write.xlsx(compiled_dataset, "compiled_WHO_GLASS_2022.xlsx")
