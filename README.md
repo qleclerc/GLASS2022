d503e11cd6ef47827f2915bb18d6eb697bf77fbd
qleclerc <quentin.leclerc@pasteur.fr>
2025-02-03 11:48:11 +0100

    added 2019 spreadsheet data

--- README.md
+++ README.md
@@ -14,23 +14,30 @@ Within this app, the plots from the report can all be recreated, and the underly
 However, each plot dataset only represents a subset of the complete GLASS dataset. Unfortunately, the complete dataset cannot be
 downloaded directly from the app, or scraped automatically from the source code.
 
-This repository essentially "recreates" the complete GLASS dataset by compiling all the subset datasets freely available via the ShinyApp.
+In addition, the 2021 GLASS report was accompanied by a spreadsheet containing all data for 2019, publicly accessible.
+
+This repository essentially attempts to "recreate" the entire GLASS dataset by compiling all the subset datasets freely available via the ShinyApp, plus the 2019 data.
+
+**Important**: this is unfortunately not the **complete** GLASS dataset, since part of it is not freely available via the app.
+Briefly, this only contains countries which have data for all years between 2017 and 2020, PLUS all data for 2019 (since this uses another spreadsheet provided by WHO).
+If a country has missed out on at least one year between 2017-2020 (except 2019), they will not appear in the compiled dataset.
 
 ## Repository contents
 
 I manually downloaded all the datasets for 91 bacteria-antibiotic combinations used to generate the "Time series of resistance to
-antibacterials (2017-2020)" plots in the app, and included them in the `raw_data` folder here. The script in `compilation_code.R` reads
+antibacterials (2017-2020)" plots in the app, and included them in the `raw_data` folder here. This folder also contains the 2019
+GLASS data (supplement of 2021 report), and a file listing WHO Regions. The script in `compilation_code.R` reads
 all these files, and generates the `compiled_WHO_GLASS_2022.csv` and `compiled_WHO_GLASS_2022.xlsx` files.
 
 Complete dataset variables:
 - **Iso3**: 3-letters country code
 - **CountryTerritoryArea**: complete country, territory, or area (CTA) name
-- **WHORegionName**: WHO region to which the CTA belongs
+- **WHORegionName**: WHO Region to which the CTA belongs
 - **Year**: data year (2017-2020)
 - **Specimen**: source of samples (blood, genital, stool, urine)
 - **PathogenName**: name of the bacteria species for which resistance is reported
 - **AbTargets**: antibiotic for which resistance is reported
-- **TotalSpecimenIsolates**: total isolates reported for this combination of year, CTA and specimen
+- **TotalSpecimenIsolates**: total isolates reported for this combination of year, CTA and specimen (bacteria and antibiotic aggregated!)
 - **InterpretableAST**: total isolates with antibiotic susceptibility reported for this combination of year, CTA, specimen, bacteria, and antibiotic
 - **Resistant**: total isolates resistant for this combination of year, CTA, specimen, bacteria, and antibiotic
 - **PercentResistant**: percent of tested isolates that are resistant (=Resistant/InterpretableAST\*100)
@@ -39,11 +46,13 @@ Complete dataset variables:
 
 Note: WHO is not responsible for the content or accuracy of the code and data in this repository.
 
-Original datasets from: Global antimicrobial resistance and use surveillance system (GLASS) report 2022. Geneva: World Health Organization; 2022. Licence: CC BY-NC-SA 3.0 IGO.
+Original datasets from:
+- Global antimicrobial resistance and use surveillance system (GLASS) report 2022. Geneva: World Health Organization; 2022. Licence: CC BY-NC-SA 3.0 IGO.
+- Global antimicrobial resistance and use surveillance system (GLASS) report 2021. Geneva: World Health Organization; 2021. Licence: CC BY-NC-SA 3.0 IGO.
 
-Code and compiled dataset by: Quentin Leclerc (quentin.leclerc@pasteur.fr; https://twitter.com/QuentinLclrc)
+Code and compiled dataset by: Quentin Leclerc (quentin.leclerc@lecnam.net; https://bsky.app/profile/quentinlclrc.bsky.social)
 
-If you wish to acknowledge this repository as the source of the compiled dataset, please cite as: Leclerc, Q.J. (2022). Compiled data from WHO GLASS 2022 report (Version 1.0) [GitHub Repository]. https://doi.org/10.5281/zenodo.7486150
+If you wish to acknowledge this repository as the source of the compiled dataset, please cite as: Leclerc, Q.J. (2022). Compiled data from WHO GLASS 2022 report (Version 2.0) [GitHub Repository]. https://doi.org/10.5281/zenodo.7486150
 
 This work is licensed under a [Creative Commons Attribution-NonCommercial-ShareAlike 3.0 IGO License](https://creativecommons.org/licenses/by-nc-sa/3.0/igo/).
 The published material is being distributed without warranty of any kind, either expressed or implied. The
--- compilation_code.R
+++ compilation_code.R
@@ -1,13 +1,21 @@
 
+# This script combines the 2019 GLASS data presented as supplement of the 2021 GLASS report
+#  with the publicly available data from the shinyapp provided with the 2022 GLASS report.
+# Briefly, the shinyapp contains greater temporal coverage, but some countries are missing.
+#  Combining with the 2019 data allows some gaps to be filled.
+
 library(here)
 library(dplyr)
 library(openxlsx)
+library(AMR)
 
 all_files <- list.files(here::here("raw_data"))
 
 compiled_dataset <- data.frame()
 
 for(file_i in all_files){
+  
+  if(file_i %in%  c("GLASS_2019.xlsx", "who-regions.csv")) next
   
   data <- read.csv(here::here("raw_data", file_i), skip = 15)
   compiled_dataset <- rbind(compiled_dataset, data)
@@ -16,7 +24,80 @@ for(file_i in all_files){
 
 compiled_dataset <- compiled_dataset %>%
   dplyr::mutate_at(vars(Year, TotalSpecimenIsolates, InterpretableAST,
-                 Resistant, PercentResistant), as.numeric)
+                        Resistant, PercentResistant), as.numeric)
 
+compiled_dataset <- compiled_dataset %>%
+  mutate(PathogenName = mo_name(as.mo(PathogenName))) %>%
+  mutate(AbTargets = ab_name(as.ab(AbTargets)))
+
+# WHO regions to align countries between datasets
+WHO_regions = read.csv(here("raw_data", "who-regions.csv")) %>%
+  select(-Year) %>%
+  rbind(data.frame(Entity = c("Kosovo", "Palestine"),
+                   Code = c("KOS", "PSE"),
+                   WHO.region = c("Europe", "Eastern Mediterranean")))
+
+# Spreadsheet included with 2021 GLASS report (2019 data only)
+df_GLASS19 = openxlsx::read.xlsx(here("raw_data", "GLASS_2019.xlsx"))
+
+df_GLASS19 = df_GLASS19 %>%
+  select(`country.(ISO-code)`, year, specimen, pathogen, antibiotic_label,
+         `Number.of.patients.with.bacterial.growth.(for.a.specific.pathogen)`,
+         `Number.of.patients.screened.for.resistance.(for.a.specific.antibiotic)`,
+         `Number.of.patients.with.resistant.results.(for.a.specific.antibiotic)`)
+colnames(df_GLASS19) = c("Code", "Year", "Specimen", "PathogenName", "AbTargets",
+                         "TotalSpecimenIsolates", "InterpretableAST", "Resistant")
+
+df_GLASS19 = df_GLASS19 %>%
+  mutate(PercentResistant = Resistant/InterpretableAST*100) %>%
+  left_join(WHO_regions, by = "Code") %>%
+  rename(Iso3 = Code, CountryTerritoryArea = Entity, WHORegionName = WHO.region) %>%
+  select(colnames(compiled_dataset)) %>%
+  mutate(WHORegionName = replace(WHORegionName, grepl("Africa", WHORegionName), "African Region"),
+         WHORegionName = replace(WHORegionName, grepl("Americas", WHORegionName), "Region of the Americas"),
+         WHORegionName = replace(WHORegionName, grepl("Eastern Mediterranean", WHORegionName), "Eastern Mediterranean Region"),
+         WHORegionName = replace(WHORegionName, grepl("Europe", WHORegionName), "European Region"),
+         WHORegionName = replace(WHORegionName, grepl("South-East Asia", WHORegionName), "South-East Asia Region"),
+         WHORegionName = replace(WHORegionName, grepl("Western Pacific", WHORegionName), "Western Pacific Region")) %>%
+  mutate(CountryTerritoryArea = replace(CountryTerritoryArea, grepl("Korea", CountryTerritoryArea), "Republic of Korea"),
+         CountryTerritoryArea = replace(CountryTerritoryArea, grepl("Russia", CountryTerritoryArea), "Russian Federation"),
+         CountryTerritoryArea = replace(CountryTerritoryArea, grepl("Laos", CountryTerritoryArea), "Lao People's Democratic Republic"),
+         CountryTerritoryArea = replace(CountryTerritoryArea, grepl("United Kingdom", CountryTerritoryArea), "United Kingdom of Great Britain and Northern Ireland"),
+         CountryTerritoryArea = replace(CountryTerritoryArea, grepl("Iran", CountryTerritoryArea), "Iran (Islamic Republic of)")) %>%
+  mutate(PathogenName = mo_name(as.mo(PathogenName))) %>%
+  mutate(AbTargets = ab_name(as.ab(AbTargets)))
+
+# In the spreadsheet, countries are sometimes split between two lines
+# A summarise step to get single estimate for country-year-source-bac-abx is therefore required
+df_GLASS19 = df_GLASS19 %>%
+  group_by(Iso3, CountryTerritoryArea, WHORegionName, Year, Specimen, PathogenName, AbTargets) %>%
+  summarise(TotalSpecimenIsolates = sum(TotalSpecimenIsolates),
+            InterpretableAST = sum(InterpretableAST),
+            Resistant = sum(Resistant)) %>%
+  ungroup %>%
+  mutate(PercentResistant = Resistant/InterpretableAST*100)
+
+correct_tot_isolates = df_GLASS19 %>%
+  group_by(CountryTerritoryArea, Year, Specimen, PathogenName) %>%
+  summarise(TotalSpecimenIsolates = max(TotalSpecimenIsolates)) %>%
+  group_by(CountryTerritoryArea, Year, Specimen) %>%
+  summarise(TotalSpecimenIsolates = sum(TotalSpecimenIsolates)) %>%
+  ungroup
+
+df_GLASS19 = df_GLASS19 %>%
+  select(-TotalSpecimenIsolates) %>%
+  left_join(correct_tot_isolates, by = c("CountryTerritoryArea", "Year", "Specimen")) %>% 
+  select(colnames(compiled_dataset))
+
+
+# Final combined dataset
+compiled_dataset <- rbind(compiled_dataset, df_GLASS19) %>%
+  distinct() %>%
+  group_by(Iso3, CountryTerritoryArea, WHORegionName, Year, Specimen, PathogenName, AbTargets) %>%
+  summarise(across(TotalSpecimenIsolates:PercentResistant, max)) %>%
+  ungroup
+
+
+# Save dataset
 write.csv(compiled_dataset, "compiled_WHO_GLASS_2022.csv", row.names = F)
 openxlsx::write.xlsx(compiled_dataset, "compiled_WHO_GLASS_2022.xlsx")
